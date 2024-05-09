6f30daf5ae7b256dcbb36b3a87b9d3b105455df4
SabihaAfreen <afrah9059@gmail.com>
2024-05-09 10:39:42 +0530

    task5commit

--- 002 01-starting-project (1)/01-starting-project/src/App.js
+++ 002 01-starting-project (1)/01-starting-project/src/App.js
@@ -1,10 +1,12 @@
 import {Fragment} from "react";
 import Meals from "./components/Meals/Meals";
 import Header from "./components/Layout/Header";
+import Cart from "./components/Cart/Cart";
 
 function App() {
   return (
     <Fragment>
+      <Cart/>
       <Header/>
       <main>
         <Meals/>
